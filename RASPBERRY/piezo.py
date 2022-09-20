31ee49983203aab48fd01ef6dda54324e88ca071
DanjelTahko <daniel.tahko@hotmail.com>
2022-09-20 19:13:09 +0100

    Created class & melody of Piezo

--- RASPBERRY/piezo.py
+++ RASPBERRY/piezo.py
@@ -1,25 +1,77 @@
 # The frequencies in this code was based on the note frequencies found at:
 # https://pages.mtu.edu/~suits/notefreqs.html
 # Tuned at A4 = 440Hz.
- 
- # imports:  
+   
 import RPi.GPIO as GPIO
 from time import sleep
  
- # setup commands:
-GPIO.setwarnings(False)              # prevents warning
-GPIO.setmode(GPIO.BOARD)             # setup I/O  
-GPIO.setup(7, GPIO.OUT)              # sets pin 7 to Output
-pin7 = GPIO.PWM(7, 100)
-pin7.start(50)
- 
- # looped commands: 
-while True:
-  GPIO.output(7, GPIO.HIGH)          # set high
-  pin7.ChangeFrequency(16.35) # C0
-  sleep(1)
-  pin7.ChangeFrequency(261.63) # C4
-  sleep(1)
-  pin7.ChangeFrequency(293.66) # D4
-  GPIO.output(7, GPIO.LOW)           # set low 
-  sleep(1)
+_NOTE_F3 = 175
+_NOTE_A3 = 220
+_NOTE_C4 = 262
+
+class Piezo:
+
+    def __init__(self):
+        self.GPIO = GPIO        
+        self.GPIO.setwarnings(False)        # prevent warnings
+        self.GPIO.setmode(GPIO.BOARD)       # setup I/O
+        self.GPIO.setup(7, GPIO.OUT)        # sets pin 7 to output
+        self.pwn = self.GPIO.PWM(7, 100)    # variable for pin 7 with frequency
+
+    def play(self):
+        
+        self.pwn.start(50) # Start imperial march
+        
+        self.pwn.ChangeFrequency(_NOTE_A3)
+        sleep(0.4)
+        self.pause()
+         
+        self.pwn.ChangeFrequency(_NOTE_A3)
+        sleep(0.4)
+        self.pause()
+
+        self.pwn.ChangeFrequency(_NOTE_A3)
+        sleep(0.4)   
+        self.pause()
+
+        self.pwn.ChangeFrequency(_NOTE_F3)
+        sleep(0.25)   
+
+        self.pwn.ChangeFrequency(_NOTE_C4)
+        sleep(0.25)
+        # ------------
+        self.pwn.ChangeFrequency(_NOTE_A3)
+        sleep(0.5)
+
+        self.pwn.ChangeFrequency(_NOTE_F3)
+        sleep(0.25)   
+
+        self.pwn.ChangeFrequency(_NOTE_C4)
+        sleep(0.25)
+
+        self.pwn.ChangeFrequency(_NOTE_A3)
+        sleep(1)
+
+        self.pwn.stop()
+
+
+    def pause(self):
+        self.pwn.stop()
+        sleep(0.1)
+        self.pwn.start(50)
+
+    
+
+
+def main():
+    # Testing purpose 
+    piezo = Piezo(GPIO)
+    while(True):
+        cin = str(input(">"))
+        if (cin == '1'):
+            piezo.play()
+        elif (cin == 'q'):
+            break
+    
+if __name__ == "__main__":
+     main()
