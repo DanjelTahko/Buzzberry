414082fedd5e45e173cc368972a2cbe591355344
DanjelTahko <daniel.tahko@hotmail.com>
2022-09-21 14:10:59 +0100

    idk lol

--- RASPBERRY/main.py
+++ RASPBERRY/main.py
@@ -1,13 +1,15 @@
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.enums import PNReconnectionPolicy
 from pubnub.pubnub import PubNub
-
+from time import sleep
 from subscriber import SubscribeHandler
 #from key_config import *
 from raspberry_keys import *
 
 def run():
     
+    # Wait for wifi connection
+    sleep(10)
     pnconfig = PNConfiguration()
     # Subscribe key for connection 
     pnconfig.subscribe_key = SUB_KEY
@@ -17,7 +19,7 @@ def run():
     pnconfig.reconnect_policy = PNReconnectionPolicy.LINEAR
     # Initialize PubNub
     pubnub = PubNub(pnconfig) # returns PubNub instance for invoking PubNub APIs.
-
+    print("Starting listener..")
     pubnub.add_listener(SubscribeHandler())
     pubnub.subscribe().channels('taco_channel').execute()
 
